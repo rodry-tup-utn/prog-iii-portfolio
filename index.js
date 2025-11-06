e19fbb9054a36b5c87014ad289ed0be6f8f8fab8
rodry <rodrymza@gmail.com>
2025-11-06 19:03:43 -0300

    cambio de icono en modo noche

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const darkModeButton = document.getElementById('boton-dark');
+document.addEventListener("DOMContentLoaded", function () {
+  const darkModeButton = document.getElementById("boton-dark");
+  const iconoDarkMode = document.getElementById("icono-dark-mode");
 
-    darkModeButton.addEventListener('click', function() {
-        document.body.classList.toggle('dark-mode');
-        console.log("Boton presionado");
-    });
+  darkModeButton.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+    if (iconoDarkMode.src.includes("night")) {
+      iconoDarkMode.src = "img/light-mode.png";
+    } else {
+      iconoDarkMode.src = "img/night-mode.png";
+    }
+  });
 });
