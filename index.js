22a51ae5e080320170f6e203fac0887de127befc
rodry <rodrymza@gmail.com>
2025-11-11 01:31:10 -0300

    cambio en color de icono de proyectos

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   const darkModeButton = document.getElementById("boton-dark");
   const iconoDarkMode = document.getElementById("icono-dark-mode");
+  const iconoGitProyecto = document.querySelectorAll('.proyecto-link__imagen');
 
   darkModeButton.addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
-    if (iconoDarkMode.src.includes("night")) {
-      iconoDarkMode.src = "img/light-mode.png";
-    } else {
-      iconoDarkMode.src = "img/night-mode.png";
-    }
+    const modoNoche = iconoDarkMode.src.includes("night");
+
+    iconoDarkMode.src = (modoNoche) ? "img/light-mode.png" : "img/night-mode.png";
+    iconoGitProyecto.forEach(element => {
+        element.src = (modoNoche) ? "img/github-light.png" : "img/github.png";
+      });
   });
 });
